624df51bffacf277e06569d1d2af66f5ffeea907
王风云 <wangfengyun@MacBook-Air-2.local>
2018-05-18 13:17:45 +0800

    删除不必要信息

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,5 @@
 export default {
   getLocalStorageItem (STORAGE_KEY) {
-    //console.log(localStorage.getItem(STORAGE_KEY))
-    //console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
     return JSON.parse(localStorage.getItem(STORAGE_KEY))
   },
   setLocalStorageItem (STORAGE_KEY, item) {
